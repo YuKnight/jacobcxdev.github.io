ce32ea1805f0d3bba588ce396f148ed2c1330d70
Jacob Clayden <jacie950@gmail.com>
2018-08-05 02:58:30 +0100

    I think I got it!

--- depictions/js/setDepiction.js
+++ depictions/js/setDepiction.js
@@ -29,7 +29,7 @@ $(function(){
                                             document.getElementById("maxiOS").innerHTML = $(this).find("maxiOS").text();
                                             
                                             $(xml).find('description').each(function(){
-                                                                            $("#description" ).textContent = ($(this).text())
+                                                                            $("#description" ).innerHTML = ($(this).text())
                                                                         });
 
                                             $(xml).find('dependency').each(function(){
