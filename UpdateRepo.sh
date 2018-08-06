aa600889124140bdeadc4cfa4f59dcc770862304
Jacob Clayden <jacie950@gmail.com>
2018-08-06 22:30:57 +0100

    Updated repo

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-rm Packages*
+rm -Rf Packages*
+mkdir Packages\ Backup
 ./dpkg-scanpackages -m . /dev/null >Packages
+cp Packages Packages\ Backup/
 bzip2 Packages
-
+cp Packages\ Backup/Packages ./
+gzip Packages
+cp Packages\ Backup/Packages ./
+rm -Rf Packages\ Backup
