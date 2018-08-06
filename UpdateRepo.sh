047bb04eec02feb587262b873093339746c749f6
Jacob Clayden <jacie950@gmail.com>
2018-08-06 23:47:15 +0100

    reeee

--- UpdateRepo.sh
+++ UpdateRepo.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-rm -Rf Packages*
-mkdir Packages\ Backup
+rm Packages*
 ./dpkg-scanpackages -m . /dev/null >Packages
-cp Packages Packages\ Backup/
 bzip2 Packages
-cp Packages\ Backup/Packages ./
-gzip Packages
-cp Packages\ Backup/Packages ./
-rm -Rf Packages\ Backup
