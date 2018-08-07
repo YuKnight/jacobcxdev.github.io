bf7fe1d8fd19e47abbc020cb2c1c17327a85e712
Jacob Clayden <jacie950@gmail.com>
2018-08-07 17:00:03 +0100

    Improved stuff

--- depictions/js/setDepiction.js
+++ depictions/js/setDepiction.js
@@ -193,13 +193,8 @@ $(function() {
                                                                         console.log("Parsed dependency: " + $(this).text());
                                                                         });
                                          
-                                         $(xml).find('linkName').each(function() {
-                                                                      $("#links").append('<li>' + $(this).text() + '</li>');
-                                                                      console.log("Parsed link: " + $(this).text());
-                                                                      });
-                                         
                                          $(xml).find('change').each(function() {
-                                                                    $("#changeLog").append('<li>' + '<h1>' + $(this).find("changeVersion").text() + '</h1>');
+                                                                    $("#changeLog").append('<li>' + '<h1>' + "v" + $(this).find("changeVersion").text() + '</h1>');
                                                                     $(this).find('changeDescription').each(function() {
                                                                                                            $("#changeLog").append('<h2>' + $(this).text() + '<h2>');
                                                                                                            console.log("Parsed changeDescription: " + $(this).text());
