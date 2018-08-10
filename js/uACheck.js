b4b8879544470bdfc498af4cd5eadab4668f6447
Jacob Clayden <jacie950@gmail.com>
2018-08-10 04:22:43 +0100

    Reee

--- js/uACheck.js
+++ js/uACheck.js
@@ -13,7 +13,7 @@ function uACheck() {
         if (cydiaSearch != null) {
             alert("Please visit this page in another browser to add the Cydia Repository");
         } else {
-            window.open("cydia://url/https://cydia.saurik.com/api/share#?source=https://jacobcxdev.github.io");
+            window.open("cydia://url/https://cydia.saurik.com/api/share#?source=https://jacobcxdev.github.io/");
         }
     } else {
         alert("Please visit this page on your iDevice to add the Cydia Repository");
