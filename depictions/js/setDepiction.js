962c24ac9fc2151a8e4262999f05683c1787d88b
Jacob Clayden <jacie950@gmail.com>
2018-08-10 04:28:38 +0100

    Fixed thing

--- depictions/js/setDepiction.js
+++ depictions/js/setDepiction.js
@@ -21,7 +21,7 @@ $(function() {
          // Parse the xml file and get data
          $(xml).find('packageInfo').each(function() {
                                          
-                                         document.getElementById("sectionIcon").src = "sections/" + $(this).find("section").text().replace(" ", "_") + ".png";
+                                         document.getElementById("sectionIcon").src = "../sections/" + $(this).find("section").text().replace(" ", "_") + ".png";
                                          document.getElementById("sectionIcon").alt = $(this).find("section").text();
                                          
                                          document.getElementById("name").innerHTML = $(this).find("name").text();
