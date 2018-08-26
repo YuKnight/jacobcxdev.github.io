d57a3505e0ea0af00bf5c67a5f7127fde183fc2a
Jacob Clayden <jacie950@gmail.com>
2018-08-26 21:56:34 +0100

    Added markdown parsing for descriptions

--- depictions/js/setDepiction.js
+++ depictions/js/setDepiction.js
@@ -7,9 +7,9 @@ $(function() {
   
   console.log(getQueryVariable('p'));
   console.log("Fetching XML");
-  var getUrl = window.location;
-  var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-  var pathTo = baseUrl + "/" + bundle;
+  var getUrl = window.location,
+  baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1],
+  pathTo = baseUrl + "/" + bundle;
   
   $.ajax({
          type: "GET",
@@ -40,24 +40,24 @@ $(function() {
                                          console.log("Parsed miniOS = " + $(this).find("miniOS").text() + " and maxiOS = " + $(this).find("maxiOS").text());
                                          
                                          
-                                         var userAgent = navigator.userAgent;
-                                         var regExp = /iP(hone|ad; CPU) OS (\d|_){1,}/;
-                                         var match = regExp.exec(userAgent);
+                                         var userAgent = navigator.userAgent,
+                                         regExp = /iP(hone|ad; CPU) OS (\d|_){1,}/,
+                                         match = regExp.exec(userAgent);
                                          
                                          if (match != null) {
-                                         var iPhoneSupported = $(this).find("iPhoneSupported").text();
-                                         var iPadSupported = $(this).find("iPadSupported").text();
-                                         var unsupportedDevice = false;
-                                         var isiPhoneOrPod = false;
-                                         var isiPad = false;
-                                         var userDevice = "unidentified";
+                                         var iPhoneSupported = $(this).find("iPhoneSupported").text(),
+                                         iPadSupported = $(this).find("iPadSupported").text(),
+                                         unsupportedDevice = false,
+                                         isiPhoneOrPod = false,
+                                         isiPad = false,
+                                         userDevice = "unidentified";
                                          
                                          var deviceString = match.shift();
                                          console.log(deviceString);
                                          var removeDevice = deviceString.replace(/(iPhone OS |iPad; CPU OS )/, "");
                                          
-                                         var iPhoneCheckRegExp = /iPhone OS /;
-                                         var iPhoneCheck = iPhoneCheckRegExp.exec(deviceString);
+                                         var iPhoneCheckRegExp = /iPhone OS /,
+                                         iPhoneCheck = iPhoneCheckRegExp.exec(deviceString);
                                          
                                          if (iPhoneCheck == null) {
                                          isiPad = true;
@@ -87,8 +87,8 @@ $(function() {
                                          var userOS = version.trim().split('.').map(Number);
                                          console.log("userOS = " + userOS);
                                          
-                                         var isLower = false;
-                                         var isHigher = false;
+                                         var isLower = false,
+                                         isHigher = false;
                                          
                                          var i = 0;
                                          
@@ -153,51 +153,53 @@ $(function() {
                                          }
                                          
                                          if (!isLower && !isHigher) {
-                                         document.getElementById("Compatibility").style["backgroundColor"] = "rgba(109, 255, 145, 1)";
-                                         document.getElementById("Compatibility").style["boxShadow"] = "rgba(109, 255, 145, 1) 0px 0px 10px";
+                                         document.getElementById("Compatibility").style.backgroundColor = "rgba(109, 255, 145, 1)";
+                                         document.getElementById("Compatibility").style.boxShadow = "rgba(109, 255, 145, 1) 0px 0px 10px";
                                          document.getElementById("youriOS").innerHTML = "Your " + userDevice + " is compatible.";
-                                         document.getElementById("compatibilityIcon").innerHTML = "😆";;
+                                         document.getElementById("compatibilityIcon").innerHTML = "😆";
                                          console.log("Your " + userDevice + " is compatible.")
                                          } else if (isLower) {
-                                         document.getElementById("Compatibility").style["backgroundColor"] = "rgba(255, 81, 81, 0.75)";
-                                         document.getElementById("Compatibility").style["boxShadow"] = "0px 0px 10px rgba(255, 81, 81, 0.75)";
+                                         document.getElementById("Compatibility").style.backgroundColor = "rgba(255, 81, 81, 0.75)";
+                                         document.getElementById("Compatibility").style.boxShadow = "0px 0px 10px rgba(255, 81, 81, 0.75)";
                                          document.getElementById("youriOS").innerHTML = "Your " + userDevice + "'s iOS version is too low, and thus is not compatible.";
                                          document.getElementById("compatibilityIcon").innerHTML = "😔";
                                          console.log("Your " + userDevice + "'s iOS version is too low.");
                                          } else if (isHigher) {
-                                         document.getElementById("Compatibility").style["backgroundColor"] = "rgba(255, 81, 81, 0.75)";
-                                         document.getElementById("Compatibility").style["boxShadow"] = "0px 0px 10px rgba(255, 81, 81, 0.75)";
+                                         document.getElementById("Compatibility").style.backgroundColor = "rgba(255, 81, 81, 0.75)";
+                                         document.getElementById("Compatibility").style.boxShadow = "0px 0px 10px rgba(255, 81, 81, 0.75)";
                                          document.getElementById("youriOS").innerHTML = "Your " + userDevice + "'s iOS version is too high, and thus is not compatible.";
                                          document.getElementById("compatibilityIcon").innerHTML = "😔";
                                          console.log("Your " + userDevice + "'s iOS version is too high.");
                                          }
                                          } else {
-                                         document.getElementById("Compatibility").style["backgroundColor"] = "rgba(255, 81, 81, 0.75)";
-                                         document.getElementById("Compatibility").style["boxShadow"] = "0px 0px 10px rgba(255, 81, 81, 0.75)";
+                                         document.getElementById("Compatibility").style.backgroundColor = "rgba(255, 81, 81, 0.75)";
+                                         document.getElementById("Compatibility").style.boxShadow = "0px 0px 10px rgba(255, 81, 81, 0.75)";
                                          document.getElementById("youriOS").innerHTML = "Your " + userDevice + " is unsupported by this package.";
                                          document.getElementById("compatibilityIcon").innerHTML = "😔";
                                          console.log("Your " + userDevice + " is unsupported by this package.");
                                          }
                                          
                                          } else {
-                                         document.getElementById("Compatibility").style["backgroundColor"] = "rgba(255, 215, 0, 0.75)";
-                                         document.getElementById("Compatibility").style["boxShadow"] = "0px 0px 10px rgba(255, 215, 0, 0.75)";
+                                         document.getElementById("Compatibility").style.backgroundColor = "rgba(255, 215, 0, 0.75)";
+                                         document.getElementById("Compatibility").style.boxShadow = "0px 0px 10px rgba(255, 215, 0, 0.75)";
                                          document.getElementById("youriOS").innerHTML = "Your device could not be identified.";
                                          document.getElementById("compatibilityIcon").innerHTML = "⚠️";
                                          console.log("Your device is unidentified.");
                                          }
                                          
-                                         var emptyRegExp = /([A-z])/i;
-                                         var descriptionEmpty = emptyRegExp.exec($(this).find("description").text());
-                                         var dependenciesEmpty = emptyRegExp.exec($(this).find("dependencies").text());
-                                         var changeEmpty = emptyRegExp.exec($(this).find("change").text());
-                                         var screenshotsEmpty = emptyRegExp.exec($(this).find("screenshots").text());
+                                         var emptyRegExp = /([A-z])/i,
+                                         descriptionEmpty = emptyRegExp.exec($(this).find("description").text()),
+                                         dependenciesEmpty = emptyRegExp.exec($(this).find("dependencies").text()),
+                                         changeEmpty = emptyRegExp.exec($(this).find("change").text());
+                                         screenshotsEmpty = emptyRegExp.exec($(this).find("screenshots").text());
                                          
                                          
                                          if (descriptionEmpty != null) {
                                          document.getElementById("descriptionBox").innerHTML = '<h1 class="boxTitle">Description</h1><div class="box"><ul id="description"></ul></div>';
                                          $(this).find('description').each(function() {
-                                                                          document.getElementById("description").innerHTML = $(this).text();
+                                                                          var converter = new showdown.Converter(),
+                                                                          html = converter.makeHtml($(this).text());
+                                                                          document.getElementById("description").innerHTML = html;
                                                                           console.log("Parsed description: " + $(this).text());
                                                                           });
                                          }
@@ -229,9 +231,9 @@ $(function() {
                                          if (screenshotsEmpty != null) {
                                          document.getElementById("screenshotsBox").innerHTML = '<h1 class="boxTitle">Screenshots</h1><div class="box"><ul id="screenshots"></ul></div>';
                                          $(this).find('screen').each(function() {
-                                                                    $("#screenshots").append('<li>' + '<img src="' + pathTo + "/" + $(this).text() + '" draggable="false" />' + '</li>');
-                                                                    console.log("Parsed screenshot: " + $(this).text());
-                                                                    });
+                                                                     $("#screenshots").append('<li>' + '<img src="' + pathTo + "/" + $(this).text() + '" draggable="false" />' + '</li>');
+                                                                     console.log("Parsed screenshot: " + $(this).text());
+                                                                     });
                                          }
                                          });
          }
@@ -248,8 +250,8 @@ $(function() {
   
   
   function getQueryVariable(variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split('&');
+  var query = window.location.search.substring(1),
+  vars = query.split('&');
   for (var i = 0; i < vars.length; i++) {
   var pair = vars[i].split('=');
   if (decodeURIComponent(pair[0]) == variable) {
