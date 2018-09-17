109a2b0131d5b0e5c150e94318f9fa443f9440b2
Jacob Clayden <jacie950@gmail.com>
2018-09-17 14:08:46 +0100

    Fixed packageList.js with S0n1c's api

--- js/packageList.js
+++ js/packageList.js
@@ -1,30 +1,26 @@
-$.getJSON("https://cydia.s0n1c.org/cydia/?fetch&url=https://jacobcxdev.github.io/", function(data) {
+$.getJSON("https://cydia.s0n1c.org/cydia/?fetch&pretty&url=https://jacobcxdev.github.io/", function(data) {
           $.each(data.packages, function(key, value) {
-                 
-                 var pathTo = value.depict.replace("?p=", ""),
-                 split = value.depict.split("/"),
+
+                 var pathTo = value[0].Depiction.replace("?p=", ""),
+                 split = value[0].Depiction.split("/"),
                  packagePath = "/depictions/" + split[split.length - 2],
                  package = null;
-                 console.log(packagePath);
-                 
+
                  $.getJSON(pathTo + "/info.json", function(data) {
-                           
+
                            var sectionIcon = "sections/" + data.section.replace(/ /g, "_") + ".png";
                            var sectionIconAlt = value.section;
-                           
+
                            var name = data.name;
-                           console.log("Parsed name = " + data.name);
                            var version = "v" + data.version;
-                           console.log("Parsed version = " + data.version);
                            var author = data.author;
-                           console.log("Parsed author = " + data.author);
-                           
+
                            var by = "";
-                           
+
                            if (author != "") {
                            by = "By";
                            }
-                           
+
                            package = {
                            sectionIcon: sectionIcon,
                            sectionIconAlt: sectionIcon,
@@ -33,9 +29,9 @@ $.getJSON("https://cydia.s0n1c.org/cydia/?fetch&url=https://jacobcxdev.github.io
                            author: author,
                            by: by
                            }
-                           
+
                            $("#packagesBox").append('<a href="' + packagePath + '"><div class="package"><h1>' + package.name + '</h1><h2 id="version">' + package.version + '</h2><img class="sectionIcon" src="' + package.sectionIcon + '" alt="' + package.sectionIconAlt + '"><h3 id="by">' + package.by + '&nbsp;</h3><h4 id="author">' + package.author + '</h4></div></a>');
-                           
+
                            });
                  });
           });
